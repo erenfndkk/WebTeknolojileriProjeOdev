8b572f4ceb71940a2bfe96934e989644c37856ab
Eren Fındık <90691429+erenfndkk@users.noreply.github.com>
2022-05-14 22:45:12 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+
+if(($_POST["email"] == "b201210061@sakarya.edu.tr") && ($_POST["password"] == "b201210061")){    
+    echo "Hoşgeldiniz";
+    }
+    else{
+        echo "Mail yada şifreniz hatalı";
+    }
+    ?>
